224a3be73288fc5e697a121b819a525039b39913
Kumar-CDACDelhi <mrankitgoyal@gmail.com>
2021-11-22 23:46:21 +0530

    job done

---
+++ src/forDemo/hello.java
@@ -0,0 +1,12 @@
+package forDemo;
+
+public class hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hi Git class");       
+
+	}
+
+}
